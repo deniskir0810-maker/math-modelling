a9bdc86124f6c65f351ea891a90d8fa81ea6df2d
deniskir0810-maker <deniskir0810@gmail.com>
2026-06-07 17:01:55 +0300

    description vec3d.hpp

--- include/vec3d.hpp
+++ include/vec3d.hpp
@@ -1,3 +1,9 @@
+/**
+* @file include/three_bodies_problem_solver.hpp
+* @author Denis Kirilenko
+* 
+* Класс трехмерного вектора
+*/
 #ifndef INCLUDE_VEC3D_HPP_
 #define INCLUDE_VEC3D_HPP_
 
@@ -5,13 +11,15 @@
 #include <cmath>
 
 namespace mm {
-
+// Точность (double/float)
 template<typename T>
     
 class R3Vec {
 private:
+  // Координаты вектора
   T x, y, z;
 public:
+  // Конструкторы
   R3Vec() {
     x = 0;
     y = 0;
@@ -35,6 +43,7 @@ public:
   T Z() const { return z; }
   T& Z() { return z; }
 
+  // Смена типа данных
   template<typename U>
   R3Vec(const R3Vec<U>& other) : x(static_cast<T>(other.X())),
     y(static_cast<T>(other.Y())),
@@ -45,6 +54,7 @@ public:
     return sqrt(x * x + y * y + z * z);
   }
 
+  // Операторы
   R3Vec operator + (const R3Vec& other) const {
     return R3Vec(x + other.x, y + other.y, z + other.z);
   }
@@ -66,12 +76,14 @@ public:
     z -= other.z;
     return *this;
   }
+  // Векторное произведение
   R3Vec operator^ (const R3Vec& other) const {
     return R3Vec(y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x);
   }
   R3Vec operator* (const T a) const {
     return R3Vec(x * a, y * a, z * a);
   }
+  // Скалярное произведение
   double operator& (const R3Vec& other) const {
     return x * other.x + y * other.y + z * other.z;
   }
