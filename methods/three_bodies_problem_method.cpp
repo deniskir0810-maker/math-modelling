5ff0fc09ba55b2defdc7f63986d5ff9a99c1a7e7
deniskir0810-maker <deniskir0810@gmail.com>
2026-06-07 17:59:39 +0300

    style check three_bodies_problem_method.cpp

--- methods/three_bodies_problem_method.cpp
+++ methods/three_bodies_problem_method.cpp
@@ -3,6 +3,7 @@
 * @author Denis Kirilenko
 */
 
+#include <string>
 #include "methods.hpp"
 #include "three_bodies_problem_solver.hpp"
 #include "abstract_solver_wrapper.hpp"
@@ -10,7 +11,8 @@
 
 namespace mm {
 
-int ThreeBodyProblemMethod(const nlohmann::json& input, nlohmann::json* output) {
+int ThreeBodyProblemMethod(const nlohmann::json& input,
+  nlohmann::json* output) {
   // Чтение общих параметров
   double tau = input.at("tau").get<double>();
   double finishTime = input.at("finishTime").get<double>();
@@ -23,7 +25,7 @@ int ThreeBodyProblemMethod(const nlohmann::json& input, nlohmann::json* output) 
     double mass = obj.at("mass").get<double>();
     const auto& pos = obj.at("position");
     const auto& vel = obj.at("velocity");
-    
+
     R3Vec<double> position(pos.at("x").get<double>(),
     pos.at("y").get<double>(),
     pos.at("z").get<double>());
@@ -31,7 +33,7 @@ int ThreeBodyProblemMethod(const nlohmann::json& input, nlohmann::json* output) 
     vel.at("y").get<double>(),
     vel.at("z").get<double>());
     R3Vec<double> acceleration(0, 0, 0);
-    
+
     return mat_point<double>(mass, position, velocity, acceleration);
   };
 
@@ -50,18 +52,17 @@ int ThreeBodyProblemMethod(const nlohmann::json& input, nlohmann::json* output) 
       static_cast<float>(exportPeriod));
 
     wrapper = new FloatAbstractSolverWrapper(solver);
-  }
-  else if (precision == "double") {
-    auto* solver = new ThreeBodyProblemSolver<double>(body0, body1, body2, tau, finishTime, exportPeriod);
+  } else if (precision == "double") {
+    auto* solver = new ThreeBodyProblemSolver<double>(body0,
+    body1, body2, tau, finishTime, exportPeriod);
     wrapper = new DoubleAbstractSolverWrapper(solver);
-  }
-  else {
-    return -1; // Неизвестная точность
+  } else {
+    return -1;  // Неизвестная точность
   }
 
   bool success = wrapper->Solve(output);
   delete wrapper;
   return success ? 0 : -1;
-}  
+}
 
-} // namespace mm
+}  // namespace mm
