82a26bcef551bc37b5bbab090765242b39858dfc
deniskir0810-maker <deniskir0810@gmail.com>
2026-06-07 18:52:10 +0300

    fixed three_body_plotter.py

--- python/plotters/three_body_plotter.py
+++ python/plotters/three_body_plotter.py
@@ -14,33 +14,33 @@ class ThreeBodyPlotter(AbstractPlotter):
         if not steps:
             raise ValueError("NO DATA FOR a VISUALIZATION")
 
-    trajectories = [[], [], []]
+        trajectories = [[], [], []]
 
-    for step in steps:
-        bodies = step["data"]["bodies"]
+        for step in steps:
+            bodies = step["data"]["bodies"]
+            for i in range(3):
+                pos = bodies[i]["position"]
+                trajectories[i].append((pos["x"], pos["y"], pos["z"]))
+
+        fig = plt.figure(figsize=(10, 8))
+        ax = fig.add_subplot(111, projection='3d')
+
+        colors = ['red', 'green', 'blue']
+        labels = ['Body 1', 'Body 2', 'Body 3']
+
         for i in range(3):
-            pos = bodies[i]["position"]
-            trajectories[i].append((pos["x"], pos["y"], pos["z"]))
+            trace = np.array(trajectories[i])
+            ax.plot(trace[:, 0], trace[:, 1], trace[:, 2],
+                    color=colors[i], linewidth=1.5, label=labels[i])
+            ax.scatter(trace[0, 0], trace[0, 1], trace[0, 2],
+                       color=colors[i], marker='o', s=50)
+            ax.scatter(trace[-1, 0], trace[-1, 1], trace[-1, 2],
+                       color=colors[i], marker='s', s=50)
 
-    fig = plt.figure(figsize=(10, 8))
-    ax = fig.add_subplot(111, projection='3d')
-
-    colors = ['red', 'green', 'blue']
-    labels = ['Body 1', 'Body 2', 'Body 3']
-
-    for i in range(3):
-        trace = np.array(trajectories[i])
-        ax.plot(trace[:, 0], trace[:, 1], trace[:, 2],
-                color=colors[i], linewidth=1.5, label=labels[i])
-        ax.scatter(trace[0, 0], trace[0, 1], trace[0, 2],
-                   color=colors[i], marker='o', s=50)
-        ax.scatter(trace[-1, 0], trace[-1, 1], trace[-1, 2],
-                   color=colors[i], marker='s', s=50)
-
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
-    ax.set_title('Trajectories of bodies moving under a gravity force')
-    ax.legend()
-    plt.savefig(self.output_path, dpi=150)
-    plt.close()
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        ax.set_title('Trajectories of bodies moving under a gravity force')
+        ax.legend()
+        plt.savefig(self.output_path, dpi=150)
+        plt.close()
